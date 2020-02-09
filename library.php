14468c7cb487c4281f834e11e7b2771a45699724
victoriavinnia <victoriavinnia@gmail.com>
2020-02-09 22:34:28 +0300

    +

--- library.php
+++ library.php
@@ -26,6 +26,7 @@ $pdo = new PDO($dsn, $login,$pwd,$options);
 $sql = "SELECT * FROM library";
 $statement = $pdo->query($sql);
 $data = $statement->fetchAll();
+//var_dump($data);
 
 ?>
 
@@ -37,11 +38,13 @@ $data = $statement->fetchAll();
 </head>
 <body>
 <form method="post">
-    <label>Какой файл Вы хотите найти?
+    <label>Поиск по названию файла:
         <input type="text" name="search_title" placeholder="Search">
         <input type="submit" name="submit_title"  value="Начать поиск">
     </label>
-    <label>Что Вы хотите найти в файле ?
+    <br>
+    <br>
+    <label>Поиск по содержимому файла:
         <input type="text" name="search_text" placeholder="Search">
         <input type="submit" name="submit_text"  value="Начать поиск">
     </label>
@@ -52,29 +55,54 @@ $data = $statement->fetchAll();
 <!--    <p>--><?// echo $book['text']; ?><!--</p>-->
 <!--</div>-->
 <?// endforeach; ?>
-<?
+<?//
 if(isset($_POST['submit_title'])) {
     $search = $_POST['search_title'];
-   // var_dump($search);
+    //var_dump($search);
     $user_sql = "SELECT * FROM library WHERE title = ?";
     $preparedStatement = $pdo->prepare($user_sql);
     $preparedStatement->bindParam(1,$search);
     $preparedStatement->execute();
     $data1 = $preparedStatement->fetch();
-   // var_dump($data1);
+    // var_dump($data1);
 }
 ?>
 <h1><? echo $data1['title']; ?></h1>
 <div>
     <p><? echo $data1['text']; ?></p>
 </div>
+
 <?
 if(isset($_POST['submit_text'])) {
-    $search = $_POST['search_text'];
-
-
+    $search_text = $_POST['search_text'];
+    //var_dump($search_text);
+    $l= mysqli_connect('127.0.0.1', 'user', '12344321', 'library');
+    $search_text = trim($search_text);
+    $q= mysqli_query($l, "SELECT * FROM `library` WHERE `text` LIKE '%$search_text%'");
+    //var_dump($q);
+    $result=mysqli_fetch_assoc($q);
+    //var_dump($result);
+    $new_data1 = $result;
+    //var_dump($new_data1);
+    $text = array_pop($new_data1);
+    //var_dump($text);
+    $res = strripos ($text, $search_text);
+    //var_dump($res);
+    mysqli_free_result($q);
+    mysqli_close($l);
 }
-
+?>
+<h1><? echo $result['title']; ?></h1>
+<div>
+    <p><? echo $result['text']; ?></p>
+</div>
+<?php
+    if ($res === false) {
+        echo "Строка '$search_text' не найдена";
+}
+    if ($res !== false) {
+        echo "Строка '$search_text' найдена в позиции $res.";
+}
 ?>
 </body>
 </html>
